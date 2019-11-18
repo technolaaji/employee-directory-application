bcdbea8da1d3b36ece32f31541473edac5ed5552
Mohamad kalaaji <technolaaji@gmail.com>
2019-11-18 13:27:17 +0200

    loader is added for signup page
    this update ensures the addition of the loader in the signup page would
    indicate if the response have arrived yet or not

--- frontend/src/components/Signup.tsx
+++ frontend/src/components/Signup.tsx
@@ -11,12 +11,14 @@ import {
 } from "../styles/signupStyle";
 import { withAlert, AlertManager } from "react-alert";
 import { postRequest } from "../util/api";
+import Loader from "./loader";
 
 class Signup extends Component<RouteComponentProps, AlertManager> {
   state = {
     email: "",
     password: "",
-    retype_password: ""
+    retype_password: "",
+    loading: false
   };
   onValueChange = e => {
     this.setState({ [e.target.name]: e.target.value });
@@ -27,6 +29,7 @@ class Signup extends Component<RouteComponentProps, AlertManager> {
       this.state.email !== "" &&
       this.state.password === this.state.retype_password
     ) {
+      this.setState({ loading: true });
       postRequest("/auth/create", {
         email: this.state.email,
         password: this.state.password
@@ -34,24 +37,29 @@ class Signup extends Component<RouteComponentProps, AlertManager> {
         .then(response => {
           if (response.data.name === "MongoError") {
             this.props.alert.show("Have you tried to log in than signing up?");
+            this.setState({ loading: false });
           } else
             this.props.alert.show(
               "User is created successfully, you can now log in to your account"
             );
+          this.setState({ loading: false });
         })
         .catch(() => {
           this.props.alert.show("An error occurred, please try again later");
+          this.setState({ loading: false });
         });
     } else {
       this.props.alert.show(
         "kindly provide all of the required information properly"
       );
+      this.setState({ loading: false });
     }
   };
 
   render() {
     return (
       <div className={signDiv}>
+        {this.state.loading ? <Loader /> : null}
         <div className={formDiv}>
           <h1 className={header}>Sign up</h1>
           <form className={formSection} onSubmit={this.onSubmit}>
